550e3764f7402b210668bb0c7950172038d7dc18
Kove W. Ochre-Salter <kove.w.o.salter>
2018-07-14 10:10:33 +1000

    Create `src/Text/Cyp/Bool.hs`

---
+++ src/Text/Cyp/Bool.hs
@@ -0,0 +1,37 @@
+
+--
+-- Copyright (c), Kove W. Ochre-Salter, 2018.
+-- This Haskell source-file is freely distributable under terms of the MIT License.
+-- A copy of said License is provided in the root directory of this project (LICENSE).
+--
+
+module Text.Cyp.Bool where
+        --
+        -- For the: `Parser` and `Stream` types.
+        --
+        import Data.Cyp
+
+        --
+        -- For the `Bool` type.
+        --
+        import Data.Bool
+
+        --
+        -- For the `Alternative` classes functions.
+        --
+        import Control.Applicative
+
+        --
+        -- Match a `Bool`.
+        --
+        matchBool :: Parser Bool
+        matchBool  = do sb <- matchString "True" <|>
+                              matchString "False"
+                        return (read sb)
+
+        --
+        -- Match the `Bool` b.
+        --
+        matchLitBool   :: Bool -> Parser Bool
+        matchLitBool b  = do s <- matchString (show b)
+                             return (read s)
