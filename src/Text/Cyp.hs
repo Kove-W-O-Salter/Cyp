8d3884ed195285de49c25473667beecfec02f0da
Kove W. Ochre-Salter <kove.w.o.salter@gmail.com>
2018-07-30 15:00:11 +1000

    Remove old, commented-out, code in 'src/Text/Cyp.hs'

--- src/Text/Cyp.hs
+++ src/Text/Cyp.hs
@@ -5,28 +5,6 @@
 --   A copy of said License is provided in the root directory of this project (LICENSE).
 --
 
--- module Text.Cyp (parse, parsePedantic) where
---         --
---         -- For foundations.
---         --
---         import Data.Cyp
--- 
---         --
---         -- | Run the `Parser` `p` on the input stream `stream`.
---         --
---         parse          :: Parser s a -> [s] -> Maybe a
---         parse p stream  = do (x, _) <- p $> stream
---                              return x
--- 
---         --
---         -- | Run the `Parser` `p` on the input stream `stream0` and fail if the resulting stream isn't empty.
---         --
---         parsePedantic          :: Parser s a -> [s] -> Maybe a
---         parsePedantic p stream0 = do (x, stream1) <- p $> stream0
---                                      if null stream1
---                                         then Just x
---                                         else Nothing
-
 module Text.Cyp (Parser,        parse,
                  parsePedantic, (<|>),
                  some,          many,
